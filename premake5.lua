409efbac9420902534b0c93edc9a358ec2622ee2
Andrew Golovnev <44880292+GolAnd071@users.noreply.github.com>
2023-03-31 06:42:17 +0300

    Updated premake file for latest version of glfw.

--- premake5.lua
+++ premake5.lua
@@ -14,12 +14,18 @@ project "GLFW"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+	
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+	
+		"src/platform.c",
         "src/vulkan.c",
         "src/window.c"
     }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
 
@@ -27,6 +33,7 @@ project "GLFW"
         {
             "src/win32_init.c",
             "src/win32_joystick.c",
+			"src/win32_module.c",
             "src/win32_monitor.c",
             "src/win32_time.c",
             "src/win32_thread.c",
