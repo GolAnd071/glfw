f475d38252857dc46a5ba6c7d35cdb2d1df6c3bd
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-17 22:50:37 +0300

    Updated premake and gitignore

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+	staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -25,28 +26,79 @@ project "GLFW"
         "src/window.c"
     }
 
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:macosx"
+		pic "On"
+
+		files
+		{
+			"src/cocoa_init.m",
+			"src/cocoa_monitor.m",
+			"src/cocoa_window.m",
+			"src/cocoa_joystick.m",
+			"src/cocoa_time.c",
+			"src/nsgl_context.m",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/osmesa_context.c",
+			"src/egl_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_COCOA"
+		}
+
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
         files
         {
             "src/win32_init.c",
-            "src/win32_joystick.c",
+			"src/win32_joystick.c",
 			"src/win32_module.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
         }
 
 		defines 
 		{ 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
+		}
+
+		links
+		{
+			"Dwmapi.lib"
 		}
 
 	filter "configurations:Debug"
@@ -56,3 +108,8 @@ project "GLFW"
     filter "configurations:Release"
         runtime "Release"
 		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
